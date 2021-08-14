66d946bd257ec6a0dcb709c696433d9690c48cac
Yaroslav Starchenkov <starchenkov.ya@gmail.com>
2021-08-15 02:11:28 +0700

    refactor: оптимизировать код

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,57 @@
-let profileElement = document.querySelector('.profile');
-let profileNameElement = profileElement.querySelector('.profile__name');
-let profileStatusElement = profileElement.querySelector('.profile__status');
-let profileEditButton = profileElement.querySelector('.profile__edit-button');
+let profileNameElement = document.querySelector('.profile__name');
+let profileStatusElement = document.querySelector('.profile__status');
+let profileEditButton = document.querySelector('.profile__edit-button');
 
 let profileEditForm = document.querySelector('.form_action_edit-profile-info');
-let profileNameInput = profileEditForm.querySelector('.form__input_data_profile-name');
-let profileStatusInput = profileEditForm.querySelector('.form__input_data_profile-status');
+let profileNameInput = document.querySelector('.form__input_data_profile-name');
+let profileStatusInput = document.querySelector('.form__input_data_profile-status');
 
 let popup = document.querySelector('.popup');
 let popupCloseButton = document.querySelector('.popup__close-button');
+
+function setProfileInfo() {
+  if (profileNameInput.value !== '' && profileStatusInput.value !== '') {
+    profileNameElement.textContent = profileNameInput.value;
+    profileStatusElement.textContent = profileStatusInput.value;
+  } else throw new Error('at least one of arguments is empty');
+}
+
+function openPopup() {
+  profileNameInput.value = profileNameElement.textContent;
+  profileStatusInput.value = profileStatusElement.textContent;
+  popup.classList.add('popup_opened');
+  profileNameInput.focus();
+}
+
+function closePopup() {
+  if (profileNameInput.placeholder !== '') profileNameInput.placeholder = '';
+  if (profileStatusInput.placeholder !== '') profileStatusInput.placeholder = '';
+  popup.classList.remove('popup_opened');
+}
+
+function profileEditFormSubmitHandler(evt) {
+  evt.preventDefault();
+  try {
+    setProfileInfo();
+    closePopup();
+  } catch(err) {
+    if (profileNameInput.value === '' && profileStatusInput.value === '') {
+      profileNameInput.placeholder = 'Введите имя!';
+      profileStatusInput.placeholder = 'Введите статус!';
+    } else if (profileNameInput.value === '') {
+      profileNameInput.placeholder = 'Введите имя!';
+    } else if (profileStatusInput.value === '') {
+      profileStatusInput.placeholder = 'Введите статус!';
+    }
+  }
+
+}
+
+profileEditButton.addEventListener('click', openPopup);
+
+popupCloseButton.addEventListener('click', closePopup);
+
+profileEditForm.addEventListener('submit', profileEditFormSubmitHandler);
 
 for (let elem of profileEditForm.elements) {
   if (elem.type === "text") {
@@ -17,34 +60,3 @@ for (let elem of profileEditForm.elements) {
     });
   }
 }
-
-profileEditButton.addEventListener('click', function() {
-  profileNameInput.value = profileNameElement.textContent;
-  profileStatusInput.value = profileStatusElement.textContent;
-  popup.classList.add('popup_opened');
-  profileNameInput.focus();
-});
-
-popupCloseButton.addEventListener('click', function() {
-  if (profileNameInput.placeholder !== '') profileNameInput.placeholder = '';
-  if (profileStatusInput.placeholder !== '') profileStatusInput.placeholder = '';
-  popup.classList.remove('popup_opened');
-});
-
-function profileEditFormSubmitHandler(evt) {
-  evt.preventDefault();
-  if (profileNameInput.value !== '' && profileStatusInput.value !== '') {
-    profileNameElement.textContent = profileNameInput.value;
-    profileStatusElement.textContent = profileStatusInput.value;
-    popup.classList.remove('popup_opened');
-  } else if (profileNameInput.value === '' && profileStatusInput.value === '') {
-    profileNameInput.placeholder = 'Введите имя!';
-    profileStatusInput.placeholder = 'Введите статус!';
-  } else if (profileNameInput.value === '') {
-    profileNameInput.placeholder = 'Введите имя!';
-  } else if (profileStatusInput.value === '') {
-    profileStatusInput.placeholder = 'Введите статус!';
-  }
-}
-
-profileEditForm.addEventListener('submit', profileEditFormSubmitHandler);
