8b83e74d15165b85846ffc246c7a6072183ad50b
Yaroslav Starchenkov <starchenkov.ya@gmail.com>
2021-09-09 19:12:40 +0700

    feat: реализовать уведовление об ошибке ввода

---
+++ scripts/validate.js
@@ -0,0 +1,58 @@
+function showInputError(settings, form, input, validationMessage) {
+  const error = form.querySelector(`.${input.id}-error`);
+
+  error.textContent = validationMessage;
+  error.classList.add(settings.errorClass);
+  input.classList.add(settings.inputErrorClass);
+}
+
+function hideInputError(settings, form, input) {
+  const error = form.querySelector(`.${input.id}-error`);
+
+  error.textContent = '';
+  error.classList.remove(settings.errorClass);
+  input.classList.remove(settings.inputErrorClass);
+}
+
+function checkInputValidity(settings, form, input) {
+  if (!input.validity.valid) {
+    showInputError(settings, form, input, input.validationMessage);
+  } else {
+    hideInputError(settings, form, input);
+  }
+}
+
+function setEventListeners(settings, form, fieldset) {
+  const inputs = Array.from(fieldset.querySelectorAll(settings.inputSelector));
+  inputs.forEach(input => {
+    input.addEventListener('input', function (evt) {
+      checkInputValidity(settings, form, input);
+    });
+  });
+}
+
+function enableValidation(settings) {
+  const forms = Array.from(document.forms);
+
+  forms.forEach(form => {
+    form.addEventListener('submit', evt => {
+      evt.preventDefault();
+    });
+
+    const fieldsets = Array.from(form.querySelectorAll(settings.fieldsetSelector));
+
+    fieldsets.forEach(fieldset => {
+      setEventListeners(settings, form, fieldset)
+    });
+  });
+}
+
+enableValidation({
+  formSelector: '.form',
+  fieldsetSelector: '.form__input-container',
+  inputSelector: '.form__input',
+  submitButtonSelector: '.form__button',
+  inactiveButtonClass: 'form__button_disabled',
+  inputErrorClass: 'form__input_type_error',
+  errorClass: 'form__input-error_active'
+});
