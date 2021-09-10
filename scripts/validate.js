30e91f6df8f428a2d6cfd2e1d225473c1d82ab53
Yaroslav Starchenkov <starchenkov.ya@gmail.com>
2021-09-11 00:19:56 +0700

    refactor: добавить модификатор _type_submit для кнопок отправки формы

--- scripts/validate.js
+++ scripts/validate.js
@@ -68,7 +68,7 @@ enableValidation({
   formSelector: '.form',
   fieldsetSelector: '.form__input-container',
   inputSelector: '.form__input',
-  submitButtonSelector: '.form__button',
+  submitButtonSelector: '.form__button_type_submit',
   inactiveButtonClass: 'form__button_inactive',
   inputErrorClass: 'form__input_type_error',
   errorClass: 'form__input-error_active'
